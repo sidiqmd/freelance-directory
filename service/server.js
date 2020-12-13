5b6cdf649d2c8145c869d0bdefcf686b640f338e
Mohamed Siddiq <siddiq@cybersolution.com.my>
2020-12-14 01:45:00 +0800

    - enable table auto create

--- service/server.js
+++ service/server.js
@@ -17,9 +17,9 @@ db.sequelize
         console.log('Connection has been established successfully to MariaDB.');
 
         // force: true will drop the table if it already exists
-        // db.sequelize.sync({ force: true }).then(() => {
-        //     console.log('Drop and Resync with { force: true }');
-        // });
+        db.sequelize.sync({ force: true }).then(() => {
+            console.log('Drop and Resync with { force: true }');
+        });
     })
     .catch(err => {
         console.error('Unable to connect to the database:', err);
